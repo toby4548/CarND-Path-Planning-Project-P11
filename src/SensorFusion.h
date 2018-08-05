ca6598a9b2014d95400b74d3e66849bfb148454d
Tony Huang <toby4548@gmail.com>
2018-08-05 22:27:56 +0200

    fix error

--- src/SensorFusion.h
+++ src/SensorFusion.h
@@ -10,8 +10,6 @@ class SensorFusion {
 public:
   // making prediction
   void Prediction(int lane, double car_s,int prev_size, float d, double vx, double vy, double check_car_s, bool& other_car_front, bool& other_car_left, bool& other_car_right);
-
-
 };
 
 
